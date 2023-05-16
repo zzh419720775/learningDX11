6c3498d8c0cc75da2087700c44c2830d1bcdcd58
zzh419720775 <419720775@qq.com>
2023-05-16 16:36:57 +0800

    Signed-off-by: zzh419720775 <419720775@qq.com>

--- Project1/WinMain.cpp
+++ Project1/WinMain.cpp
@@ -1,18 +1,42 @@
 #include <Windows.h>
+#include "WindowsMessageMap.h"
 
+LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	static WindowsMessageMap mm;
 
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'D') {
+			SetWindowText(hWnd, "Respects");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, "Dangerfield");
+		}
+		break;
+	default:
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
-	const auto pClassName = L"hw3dbutts";
+	const auto pClassName = "hw3dbutts";
 	//register window class 
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WinProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -24,12 +48,26 @@ int CALLBACK WinMain(
 	wc.hIconSm = nullptr;
 	RegisterClassEx(&wc);
 	HWND hWnd = CreateWindowEx(0, pClassName,
-		L"happy hard window",
+		"happy hard window",
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
 		200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr);
 	//show the window
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
-	return 0;
+
+	//message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
