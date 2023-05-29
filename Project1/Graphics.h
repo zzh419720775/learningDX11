aa8b13dca0fa8e4b6849d195533b3a9eee4305b3
zzh419720775 <419720775@qq.com>
2023-05-29 17:39:52 +0800

    Signed-off-by: zzh419720775 <419720775@qq.com>

--- Project1/Graphics.h
+++ Project1/Graphics.h
@@ -1,9 +1,36 @@
 #pragma once
 #include "ChiliWin.h"
+#include "ChiliException.h"
 #include <d3d11.h>
-
+#include <string>
+#include <vector>
+#include "DxgiInfoManager.h"
 class Graphics {
 public:
+	class Exception : public ChiliException {
+		using ChiliException::ChiliException;
+	};
+	class HrException : public Exception {
+	public:
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
+		const char* what()const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		HRESULT hr;
+		std::string info;
+
+	};
+	class DeviceRemovedException : public HrException {
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
+	};
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
 	Graphics& operator=(const Graphics&) = delete;
@@ -11,6 +38,10 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
+#ifndef NODEBUG
+	DxgiInfoManager infoManager;
+#endif // !NODEBUG
+
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
--- Project1/Window.cpp
+++ Project1/Window.cpp
@@ -4,7 +4,7 @@
 //Window Class Stuff
 Window::WindowClass Window::WindowClass::wndClass;
 
-Window::WindowClass::WindowClass() noexcept 
+Window::WindowClass::WindowClass() noexcept
 	:
 	hInst(GetModuleHandle(nullptr))
 {
@@ -24,7 +24,7 @@ Window::WindowClass::WindowClass() noexcept
 	RegisterClassEx(&wc);
 }
 
-Window::WindowClass::~WindowClass() 
+Window::WindowClass::~WindowClass()
 {
 	UnregisterClass(wndClassName, GetInstance());
 }
@@ -49,7 +49,7 @@ Window::Window(int width, int height, const char* name) noexcept
 	wr.right = width + wr.left;
 	wr.top = 100;
 	wr.bottom = height + wr.top;
-	
+
 	if (AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE) == 0)
 	{
 		throw CHWND_LAST_EXCEPT();
@@ -75,11 +75,11 @@ void Window::SetTitle(const std::string& title)
 	}
 }
 
-std::optional<int> Window::ProcessMessages()
+std::optional<int> Window::ProcessMessages() noexcept
 {
 	MSG msg;
 	//while queue has messages, remove and dispatch them(but do not block on empty queue)
-	while ( PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 	{
 		//check for quit becaust peekMessages does not signal this via return val
 		if (msg.message == WM_QUIT) {
@@ -96,6 +96,9 @@ std::optional<int> Window::ProcessMessages()
 
 Graphics& Window::Gfx()
 {
+	if (!pGfx) {
+		throw CHWND_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -134,13 +137,13 @@ LRESULT CALLBACK Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 		PostQuitMessage(0);
 		return 0;
 
-	// clear keystate when window loses focus to prevent input getting "stuck"
-	case WM_KILLFOCUS: 
+		// clear keystate when window loses focus to prevent input getting "stuck"
+	case WM_KILLFOCUS:
 		kbd.ClearState();
 		break;
-	/*******************  Keyboard Messages  ********************/
+		/*******************  Keyboard Messages  ********************/
 	case WM_KEYDOWN:
-	// syskey commands need to be handled to track ALT key (VK_MENU) and F10
+		// syskey commands need to be handled to track ALT key (VK_MENU) and F10
 	case WM_SYSKEYDOWN:
 		if (!(lParam & 0x40000000) || kbd.AutorepeatIsEnabled()) {
 			kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
@@ -167,13 +170,13 @@ LRESULT CALLBACK Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 			}
 		}
 		// not in client -> log move / maintain capture if button down
-		else 
+		else
 		{
 			if (wParam & (MK_LBUTTON | MK_RBUTTON)) {
 				mouse.OnMouseMove(pt.x, pt.y);
 			}
 			// button up -> release capture / log event for leaving
-			else 
+			else
 			{
 				ReleaseCapture();
 				mouse.OnMouseLeave();
@@ -233,32 +236,10 @@ LRESULT CALLBACK Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 }
 
 // Window Exception Stuff
-Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
-	:
-	ChiliException(line, file),
-	hr(hr)
-{}
-
-const char* Window::Exception::what() const noexcept
-{
-	std::ostringstream oss;
-	oss << GetType() << std::endl
-		<< "[Error Code] " << GetErrorCode() << std::endl
-		<< "[Description] " << GetErrorString() << std::endl
-		<< GetOriginString();
-	whatBuffer = oss.str();
-	return whatBuffer.c_str();
-}
-
-const char* Window::Exception::GetType() const noexcept
-{
-	return "Chili Window Exception";
-}
-
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
 	char* pMsgBuf = nullptr;
-	DWORD nMsgLen = FormatMessage(
+	const DWORD nMsgLen = FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER |
 		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 		nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
@@ -272,14 +253,42 @@ std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 	LocalFree(pMsgBuf);
 	return errorString;
 }
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	:
+	Exception(line, file),
+	hr(hr)
+{}
 
-HRESULT Window::Exception::GetErrorCode() const noexcept
+const char* Window::HrException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+
+}
+
+const char* Window::HrException::GetType() const noexcept
+{
+	return "Chili Window Exception";
+}
+
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-std::string Window::Exception::GetErrorString() const noexcept
+std::string Window::HrException::GetErrorDescription() const noexcept
 {
-	return TranslateErrorCode(hr);
+	return Exception::TranslateErrorCode(hr);
+}
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "Chili Window Exception [No Graphics]";
 }
 
