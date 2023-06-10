725b0aee1a6f429a60c9ac490ba7c35dae709e5e
zhang <419720775@qq.com>
2023-06-10 10:08:20 +0800

    Signed-off-by: zhang <419720775@qq.com>

--- Project1/ColorBlendVS.hlsl
+++ Project1/ColorBlendVS.hlsl
@@ -4,13 +4,14 @@ cbuffer Cbuf {
 
 struct VSout {
 	float4 color : Color;
-	float4 pos : SV_Position
+	float4 pos : SV_Position;
 
 };
+
 VSout main(float3 pos : Positon, float color : Color) : SV_POSITION
 {
 	VSout vso;
-	vso.pos = mul(float(pos, 1.f), transform);
+	vso.pos = mul(float4(pos, 1.f), transform);
 	vso.color = color;
-	return pos;
+	return vso;
 }
